7d514c6a287596b1a52dd87037d7d0140dc2e7f3
KevinJared <kevinjaredkip@gmail.com>
2018-05-23 22:33:36 +0300

    styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
 $(document).ready(function() {
-        $("#myform").submit(function() {
-          $(".name").append("");
-          $(".email").append("");
 
-          $("#story").show();
-        });
-      });
+  var modal = document.getElementById('myModal');
+
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  $("#login form").submit(function() {
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+
+    $(".name").append(nameInput);
+    $(".email").append(emailInput);
+
+  });
+});
